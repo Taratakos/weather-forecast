947b55319d826d72d0834d2317b4d5d19aed7706
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-05-16 17:18:05 +0300

    feat: add temperature feature (Tue May 16 2023 17:17:31 PM)

--- src/script.js
+++ src/script.js
@@ -47,7 +47,7 @@ dateListItem.innerHTML = formatDate(currentTime);
 
 /*
 Challenge 2
-So we basically want to say, every time I type something in city-input it shoul show the text which I typed
+So we basically want to say, every time I type something in city-input it should show the text which I typed
 */
 function showCityName(event) {
   event.preventDefault();
@@ -57,3 +57,32 @@ function showCityName(event) {
 
 let searchForm = document.querySelector('.search-form');
 searchForm.addEventListener('submit', showCityName);
+
+/*
+Challenge 3
+*/
+function convertToFahrenheit(event) {
+  event.preventDefault(); // because we don't want to link to anything
+  // we want ta access to this tempElement
+  let temperatureElement = document.querySelector('#temperature');
+  // let temperature = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault(); // because we don't want to link to anything
+  // we want ta access to this tempElement
+  let temperatureElement = document.querySelector('#temperature');
+  // let temperature = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = 19;
+}
+
+let temperatureElement = document.querySelector('#temperature');
+let temperature = temperatureElement.innerHTML;
+temperature = Number(temperature);
+
+let fahrenheitLink = document.querySelector('#fahrenheit-link');
+fahrenheitLink.addEventListener('click', convertToFahrenheit);
+
+let celsiusLink = document.querySelector('#celsius-link');
+celsiusLink.addEventListener('click', convertToCelsius);
